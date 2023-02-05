4a4e83972da737213b58de4887b7582351a0abef
sousaua <hsousa@ua.pt>
2023-02-05 22:11:53 -0100

    Create find mission server

---
+++ src/robutler_controller/src/find_server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import roslib
+roslib.load_manifest('robutler_controller')
+import rospy
+import actionlib
+from robutler_controller.msg import FindAction
+
+class FindServer:
+  def __init__(self):
+    self.server = actionlib.SimpleActionServer('find', FindAction, self.execute, False)
+    self.server.start()
+
+  def execute(self, goal):
+    rospy.loginfo(f'Executing find {goal.objectType} in {goal.room}')
+    self.find(goal)
+    self.server.set_succeeded()
+
+
+  def find(self, goal):
+    rospy.loginfo('Finding...')
+
+
+    rospy.loginfo(f'Found {goal.objectType} in {goal.room}')
+
+
+
+if __name__ == '__main__':
+  rospy.init_node('find_server')
+  server = FindServer()
+  rospy.spin()
