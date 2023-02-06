7aaf16a3b917cb71a30d8a5846c4a80dc4a71361
sousaua <hsousa@ua.pt>
2023-02-06 21:36:29 -0100

    Spawning colored cubes dynamically

--- src/psr_apartment_description/src/spawn_object.py
+++ src/psr_apartment_description/src/spawn_object.py
@@ -15,18 +15,24 @@ rospack = rospkg.RosPack()
 package_path = rospack.get_path('psr_apartment_description') + '/description/models/'
 
 
-#Add here mode poses
-placements = []
-placements.append({'pose':Pose(position=Point(x=-5.69, y=4.37, z=0.6), orientation=Quaternion(x=0,y=0,z=0,w=1)),
-              'room':'large_bedroom', 'place': 'bed'})
-placements.append({'pose':Pose(position=Point(x=-7.33, y=5.29, z=0.58), orientation=Quaternion(x=0,y=0,z=0,w=1)),
-              'room':'large_bedroom', 'place': 'bedside_cabinet'})
-
-
 model_names = ['blue_cube', 'green_cube', 'red_cube']
 
-places = ["In the bed of the large bedroom", "In the living room shelf"]
+places = ["In the bed of the large bedroom", "In the living room shelf", "In the small room furniture", "In the kitchen cabinet"]
 
+places_pose = {place: None for place in places}
+places_pose['In the bed of the large bedroom'] = [{'pose':Pose(position=Point(x=-5.69, y=4.37, z=1), orientation=Quaternion(x=0,y=0,z=0,w=1)),'room':'large_bedroom', 'place': 'bed'}]
+places_pose['In the living room shelf'] = [{'pose':Pose(position=Point(x=-2.169804, y=-2.422212, z=0.115565), orientation=Quaternion(x=0,y=0,z=0,w=1)),'room':'sala', 'place': 'shelf'},
+    {'pose':Pose(position=Point(x=-1.530873, y=-2.383779, z=0.115565), orientation=Quaternion(x=0,y=0,z=0,w=1)),'room':'sala', 'place': 'shelf'},
+    {'pose':Pose(position=Point(x=-3.401368, y=-2.338209, z=0.495562), orientation=Quaternion(x=0,y=0,z=0,w=1)),'room':'sala', 'place': 'shelf'},
+    {'pose':Pose(position=Point(x=-2.694710, y=-2.400191, z=0.865564), orientation=Quaternion(x=0,y=0,z=0,w=1)),'room':'sala', 'place': 'shelf'},
+    {'pose':Pose(position=Point(x=-3.468925, y=-2.386329, z=1.255566), orientation=Quaternion(x=0,y=0,z=0,w=1)),'room':'sala', 'place': 'shelf'},
+    {'pose':Pose(position=Point(x=-1.761279, y=-2.208371, z=1.255566), orientation=Quaternion(x=0,y=0,z=0,w=1)),'room':'sala', 'place': 'shelf'}]
+places_pose['In the small room furniture'] = [{'pose':Pose(position=Point(x=-1.861356, y=3.959174, z=0.5), orientation=Quaternion(x=0,y=0,z=0,w=1)),'room':'small_room', 'place': 'furniture'}]
+places_pose['In the kitchen cabinet'] = [{'pose':Pose(position=Point(x=-3.172834, y=-1.785831, z=0.956774), orientation=Quaternion(x=0,y=0,z=0,w=1)),'room':'kitchen', 'place': 'cabinet'},
+    {'pose':Pose(position=Point(x=-1.443919, y=-0.811314, z=0.956774), orientation=Quaternion(x=0,y=0,z=0,w=1)),'room':'kitchen', 'place': 'cabinet'},]
+
+
+count = 0
 while True:
     if len(places) == 0:
         print("There is nothing more you can do")
@@ -42,26 +48,24 @@ while True:
         print("Exiting...")
         sys.exit(0)
     
-    if place_idx < 0 or place_idx > len(placements):
+    if place_idx < 0 or place_idx > len(places):
         print("Invalid place number")
         continue
 
     place = places.pop(place_idx-1)
+    poses = places_pose[place]
 
-    print(place)
-    continue
+    for pose in poses:
+        model_name = random.choice(model_names)
 
+        f = open( package_path + model_name + '/model.sdf' ,'r')
+        sdff = f.read()
 
-    # Add here several models. All should be added to the robutler_description package
-    model_name = random.choice(model_names)
+        rospy.wait_for_service('gazebo/spawn_sdf_model')
+        spawn_model_prox = rospy.ServiceProxy('gazebo/spawn_sdf_model', SpawnModel)
 
-    f = open( package_path + model_name + '/model.sdf' ,'r')
-    sdff = f.read()
-
-    rospy.wait_for_service('gazebo/spawn_sdf_model')
-    spawn_model_prox = rospy.ServiceProxy('gazebo/spawn_sdf_model', SpawnModel)
-
-
-    model_placement = random.choice(placements)
-    name = model_name + '_in_' + model_placement['place'] + '_of_' + model_placement['room']
-    spawn_model_prox(name, sdff, model_name, model_placement['pose'], "world")
+        model_placement = pose
+        name = model_name + '_in_' + model_placement['place'] + '_of_' + model_placement['room'] + '_' + str(count)
+        spawn_model_prox(name, sdff, model_name, model_placement['pose'], "world")
+        print("Placed " + name)
+        count += 1
